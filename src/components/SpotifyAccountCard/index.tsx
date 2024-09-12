ac5a7e377cb8a23bc26bb28e69fe43a9c02d141b
Greyson Elkins <greysonelkins@gmail.com>
2024-09-12 16:39:35 -0600

    Start replicating Spotify Style

--- src/components/SpotifyAccountCard/index.tsx
+++ src/components/SpotifyAccountCard/index.tsx
@@ -4,12 +4,10 @@ import Account from './Account'
 
 import style from './SpotifyAccountCard.module.scss'
 
-
 const SpotifyAccountCard = () => {
   return (
     <div className={style.Account}>
       <h2>Spotify</h2>
-      <hr />
       <Account />
     </div>
   )
